b97eb5a292c84aae2ce8ebf4e0a56b48bec051eb
Josh Tan <jtan189@gmail.com>
2013-11-16 16:28:22 -0600

    Added initial program.

---
+++ TanAssig4.R
@@ -0,0 +1,11 @@
+## R program for determining topological properties of a network.
+##
+## Input requirements:   the name of the file
+##   dataFile: filename of data for which to determine topological properties
+##
+## Josh Tan
+## CSCI 479
+## 11/21/13
+
+## INPUT VARIABLES
+dataFile = "networkDatasets/toyN.txt"
